3046aa166884647bd5627c8ad3df50cce54515f4
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-02-27 00:15:18 +0900

    update WelcomeMessage for 3/3 study!

--- app/models/WelcomeMessage.java
+++ app/models/WelcomeMessage.java
@@ -4,6 +4,6 @@ import play.db.jpa.Model;
 
 public class WelcomeMessage extends Model {
 	public String getMessage() {
-		return "Hello! Play Framework!!よろしくーへろくー今日は勉強会！";
+		return "Hello! Play Framework!!3/3は勉強会！";
 	}
 }
