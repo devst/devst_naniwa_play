0cc91adc78ef02293cf35d0b77be41a201d9b97f
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-02-27 02:34:28 +0900

    test fail pattern 3

--- test/models/WelcomeMessageTest.java
+++ test/models/WelcomeMessageTest.java
@@ -8,7 +8,7 @@ public class WelcomeMessageTest {
 
 	@Test
 	public void testGetMessage() {
-		assertEquals("Hello! Play Framework!!3/3は勉強会ですよ_1", new WelcomeMessage().getMessage());
+		assertEquals("Hello! Play Framework!!3/3は勉強会ですよ", new WelcomeMessage().getMessage());
 	}
 
 }
