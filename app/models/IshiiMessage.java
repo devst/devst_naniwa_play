2ee6f2b4b82a36e154a15e66cf52bfa5a4611b58
gu <gu@example.com>
2012-03-03 14:09:38 +0900

    add IshiiMessage class again.

--- app/models/IshiiMessage.java
+++ app/models/IshiiMessage.java
@@ -2,7 +2,7 @@ package models;
 
 import play.db.jpa.Model;
 
-public class WelcomeMessage extends Model {
+public class IshiiMessage extends Model {
 	public String getMessage() {
 		return "こんにちは、石井です！";
 	}
